e5d4231a3999913630850b56a0bd7fbe275f8f73
Khalil Saboor <khalilsaboor1@gmail.com>
2023-01-20 12:52:29 -0500

    part one

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const http = require('http');
 
-const PORT = 5000;
+const PORT = 5001;
 const server = http.createServer((req, res) => {
     if (req.method === 'GET' && req.url === "/")
         res.statusCode = 200;
         res.end();
 })
 
-server.listen(PORT, (err, res) => {
-    console.log("Server listening on port" + PORT);
-    console.log(`Server on http://localhost:{PORT}`)
+server.listen(PORT, () => {
+    console.info("Server listening on port " + PORT);
+    console.log(`Server on http://localhost:${PORT}`)
 })
